b5b64f2a848629de2f62a577008e9b69aeb3c710
Thiago Batista <Thiagobatistadiass@gmail.com>
2024-04-08 17:42:44 -0300

    atualizado as imagem

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const box = document.querySelector("container");
-const imagens = document.querySelector(".container img");
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
 
 let contador = 0;
 
 function slider(){
     contador ++;
+    
+    if(contador > imagens.length - 1){
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 1250}px)`
 }
 
-setinterval(slider , 2000);
+setInterval(slider , 2000);
